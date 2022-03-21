8bb767410d3c14939210ba018fb6c8457a681651
Ryan.li <Ryan.li@cygia.com>
2022-03-21 16:46:32 +0800

    # 测试 vuepress blog theam

--- rawDocs/.vuepress/config.js
+++ rawDocs/.vuepress/config.js
@@ -1,6 +1,24 @@
 module.exports = {
-    title: 'Hello VuePress',
+    title: 'Ryan\'s Blog',
     description: 'Just playing around',
     dest:  '.\\docs',
-    base:"."
+    // base:"."  ,//部署到github 上时，本行注释应去掉
+
+    // plugins: [
+    //   [
+    //     '@vuepress/blog',
+    //     {
+    //       directories: [
+    //         {
+    //           // Unique ID of current classification
+    //           id: 'post',
+    //           // Target directory
+    //           dirname: '_posts',
+    //           // Path of the `entry page` (or `list page`)
+    //           path: '/',
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // ],
   }
